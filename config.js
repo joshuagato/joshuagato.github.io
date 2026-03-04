461f1595767c01131a876c560d0e86f0d30ccedc
Joshua Gato <joshuagato37@gmail.com>
2026-03-04 20:21:06 +0000

    Updated code to detect all browswers

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-//     API_URL: "http://127.0.0.1:8000/api/capture-visitor/",
+     API_URL: "http://127.0.0.1:8000/api/capture-visitor/",
     API_URL: "https://datacollector.joshuagato.online/api/capture-visitor/",
 };
